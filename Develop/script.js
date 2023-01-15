819d54b2c5ea8691837d37174464cae7c85a5e7c
waldenlight <76961678+waldenlight@users.noreply.github.com>
2023-01-15 13:17:23 -0700

    Refactor code

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,5 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-
+  // Saving events
   function saveEvent(event) {
     var currentEvent = $(event.target);
     var eventText = currentEvent.parent('div').children('textarea');
@@ -21,12 +12,7 @@ $(function () {
 
   $('.saveBtn').click(saveEvent)
 
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-
+  // Color coordination
   var currentHour = dayjs().format("H");
 
   // Loop through all the time blocks and add the appropriate class
@@ -53,28 +39,25 @@ $(function () {
     }
   }
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  // Populate events
+  function populateEvents() {
+    for (i = 0; i < 11; i++) {
+      // Get the id of the event
+      eventId = $(".container-fluid").children('.row').eq(i).attr('id');
+      // Get the current event local storage data
+      currentEventLocalObject = JSON.parse(localStorage.getItem(eventId));
+      // Get the text area for the event
+      currentEventText = $(".container-fluid").children('.row').eq(i).children('textarea');
+      // If local storage objects exist, access text and populate
+      if (currentEventLocalObject) {
+        currentEventText.text(currentEventLocalObject.text);
+      }
+    }
+  }
+
+  populateEvents();
+
+  // Display current date
   var currentDate = dayjs().format('MMM DD, YYYY');
   $("#currentDay").text(currentDate);
 });
-
-function populateEvents() {
-  for (i = 0; i < 10; i++) {
-    // Get the id of the event
-    eventId = $(".container-fluid").children('.row').eq(i).attr('id');
-    // Get the current event local storage data
-    currentEventLocalObject = JSON.parse(localStorage.getItem(eventId));
-    // Get the text area for the event
-    currentEventText = $(".container-fluid").children('.row').eq(i).children('textarea');
-    // If local storage objects exist, access text and populate
-    if (currentEventLocalObject) {
-      currentEventText.text(currentEventLocalObject.text);
-    }
-  }
-}
-
-populateEvents();
